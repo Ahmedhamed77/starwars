059540edc5cfdbad8490c22ab1097eacd28bd599
AHMED <ahmedhamed7728@gmail.com>
2021-07-25 22:24:50 +0700

    footer

--- src/Route/root.tsx
+++ src/Route/root.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { Footer } from '../components/Footer';
 import {FavoriteScreen} from '../screens/FavoriteScreen';
 import {MainScreen} from '../screens/MainScreen';
 
@@ -9,6 +10,7 @@ export const Root = () => {
       <Switch>
         <Route exact path="/">
           <MainScreen />
+          <Footer/>
         </Route>
         <Route exact path="/favorite">
           <FavoriteScreen />
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './footer.css'
+export const Footer = () => {
+    return (
+        <div className='footer'>
+            <p>Made with ♥️ Ahmed Hamed</p>
+        </div>
+    )
+}
+
