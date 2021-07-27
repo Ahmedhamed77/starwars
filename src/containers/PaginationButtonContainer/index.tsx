69df058a25b5a3e32d49cd078445e599cb9ad2ca
AHMED <ahmedhamed7728@gmail.com>
2021-07-27 18:34:02 +0700

    prev next page

--- src/containers/PaginationButtonContainer/index.tsx
+++ src/containers/PaginationButtonContainer/index.tsx
@@ -5,34 +5,16 @@ import {fetchCharactersData} from '../../redux/hero/actions';
 import {Store} from '../../redux/store/types';
 
 export interface ButtonPaginationProps {
-  page: number;
-  setPage: any;
+
+  handleNextPage:any,
+  handlePrevPage:any,
 }
 export const Pagination: React.FC<ButtonPaginationProps> = ({
-  page,
-  setPage,
+  handleNextPage,
+  handlePrevPage,
 }) => {
   const dispatch = useDispatch();
   const isLoading = useSelector((store: Store) => store.hero.heroIsLoading);
-
-  const handleNextPage = () => {
-    if (page === 9) {
-      setPage(9);
-      dispatch(fetchCharactersData(9));
-    } else {
-      setPage(page + 1);
-      dispatch(fetchCharactersData(page + 1));
-    }
-  };
-  const handlePrevPage = () => {
-    if (page <= 1) {
-      setPage(1);
-      dispatch(fetchCharactersData(1));
-    } else {
-      setPage(page - 1);
-      dispatch(fetchCharactersData(page - 1));
-    }
-  };
   return (
     <ButtonPagination
       isLoading={isLoading}
--- src/screens/MainScreen/index.tsx
+++ src/screens/MainScreen/index.tsx
@@ -2,32 +2,50 @@ import React, {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 
 import {ActivityIndicator} from '../../components/ActivityIndicator';
-import {
-  fetchCharactersData,
-} from '../../redux/hero/actions';
+import {fetchCharactersData} from '../../redux/hero/actions';
 import {Store} from '../../redux/store/types';
 
 import './styles.css';
 import {Navbar} from '../../components/NavBar';
 import {SearchBar} from '../../components/searchbar';
 import {FilterButton} from '../../components/FilterButton';
-import { Pagination } from '../../containers/PaginationButtonContainer';
-import { CardHeroContainer } from '../../containers/CardHeroContainer';
+import {Pagination} from '../../containers/PaginationButtonContainer';
+import {CardHeroContainer} from '../../containers/CardHeroContainer';
 
 export const MainScreen = () => {
   const dispatch = useDispatch();
-  const {heroes, heroIsLoading} = useSelector(
-    (store: Store) => store.hero,
-  );
+  const {heroes, heroIsLoading} = useSelector((store: Store) => store.hero);
+  const {favorites} = useSelector((store: Store) => store.heroFavorites);
+
   const [query, setQuery] = useState('');
   const [data, setData] = useState(heroes);
-  const [page,setPage] = useState(1);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
-    !heroes.length && dispatch(fetchCharactersData(1));
+     !heroes.length && dispatch(fetchCharactersData(page));
     setData(heroes);
-  }, [heroes]);
+  }, [heroes,favorites]);
 
+
+  const handleNextPage = () => {
+    if (page === 9) {
+      setPage(9);
+      dispatch(fetchCharactersData(9));
+    } else {
+      setPage(page => page + 1);
+      dispatch(fetchCharactersData(page));
+    }
+  };
+  const handlePrevPage = () => {
+    if (page <= 1) {
+      setPage(1);
+      dispatch(fetchCharactersData(1));
+    } else {
+      setPage(page - 1);
+      dispatch(fetchCharactersData(page));
+    }
+  };
+  
   let uniques = Array.from(new Set(heroes.map(hero => hero.gender)));
   let categories = ['all', ...uniques];
   const filterItems = (category: any) => {
@@ -42,7 +60,6 @@ export const MainScreen = () => {
   return (
     <div>
       <Navbar />
-    
       <SearchBar heroes={heroes} query={query} setQuery={setQuery} />
       <FilterButton categories={categories} filterItems={filterItems} />
       {heroIsLoading ? (
@@ -51,14 +68,12 @@ export const MainScreen = () => {
         <div>
           <ul className="container">
             {data.map((hero, index) => {
-              return <CardHeroContainer key={index} hero={hero}  />;
+              return <CardHeroContainer key={index} hero={hero} />;
             })}
           </ul>
-          <Pagination page={page} setPage={setPage}/>
+          <Pagination handleNextPage={handleNextPage} handlePrevPage={handlePrevPage} />
         </div>
-        
       )}
-      
     </div>
   );
 };
